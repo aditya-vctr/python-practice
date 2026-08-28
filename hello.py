9cfca9851dc125e62f04a5f063acc61383c7a643
aditya-vctr <adityaraj77716@gmail.com>
2026-08-28 20:58:39 +0530

    Update hello program

--- hello.py
+++ hello.py
@@ -1,23 +1,6 @@
-# name=(input("What is your name?: "))
+name = input("Enter your name: ")
 
-# print(f"hello {name}")
-
-# age = int(input('How old are you? :'))
-# print(f"You are {age} years old")
-
-#excercise 1 rectangle area calculator
-
-# length = float(input("Enter the length in meters: "))
-# breadth = float(input("Enter the breadth in meters: "))
-# area = length * breadth
-# print(f"Area of Rectangle is: {area} m²")
-
-#Excercise 2 Shopping cart program
-
-item = input("What item would you like to buy?: ")
-price = float(input("What is the price?: "))
-quantity = int(input("How many would you like?: "))
-total = price * quantity
-print(f"your total price is ₹{total}")
+print(f"Hello, {name}!")
+print("You are learning Python and GitHub.")
 
 
