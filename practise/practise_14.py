8aaff5e46a64462e0369b27fa425bac72f9afe58
aditya-vctr <adityaraj77716@gmail.com>
2026-08-29 11:35:50 +0530

    Practiced string modification

---
+++ practise/practise_14.py
@@ -0,0 +1,22 @@
+def modify_string_1(s: str) -> str:
+    '''
+    Given a string, Seperate the characters present in odd and even indices
+    and return the merged string with even indices first and odd indices second in reverse order.
+
+    Arguments:
+    s: str - the input string
+
+    Return:
+    str - modified string
+
+    Example:
+    >>> modify_string_1('abcde')
+    'acedb'
+    >>> modify_string_1('python')
+    'ptonhy'
+    '''
+    even = s[::2] # Grab all the even indices
+    odd = s[1::2] # Grab all the odd indices
+    reversed_odd = odd[::-1]
+    return even + reversed_odd
+print(modify_string_1('Indian'))
