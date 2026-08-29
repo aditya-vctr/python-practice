1c56cbeb1753219300ec7cb6dcccd28744efdb43
Aditya Raj <adityaraj77716@gmail.com>
2026-08-29 11:44:57 +0530

    Change input string in modify_string_1 call

--- practice/practice_14.py
+++ practice/practice_14.py
@@ -19,4 +19,4 @@ def modify_string_1(s: str) -> str:
     odd = s[1::2] # Grab all the odd indices
     reversed_odd = odd[::-1]
     return even + reversed_odd
-print(modify_string_1('Indian'))
+print(modify_string_1('python'))
