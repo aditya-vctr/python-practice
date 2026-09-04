d6803e05d544e74993f720f3bd6ec83ea1d3256b
aditya-vctr <adityaraj77716@gmail.com>
2026-09-04 20:00:36 +0530

    Check if even two digit number

---
+++ practice/practice_25.py
@@ -0,0 +1,30 @@
+#  Check If Even Two-Digit Number
+def is_even_two_digit_number(num):
+    """
+    Determines whether a given number is an even two-digit number.
+
+    Args:
+        num (int): The number to check.
+
+    Returns:
+        bool: True if `num` is an even two-digit number, False otherwise.
+
+    Examples:
+        >>> is_even_two_digit_number(42)
+        True
+        >>> is_even_two_digit_number(99)
+        False
+        >>> is_even_two_digit_number(-48)
+        True
+        >>> is_even_two_digit_number(5)
+        False
+    """
+    is_two_digit =  10 <= abs(num) <= 99
+    is_even = num % 2 == 0
+
+    if is_two_digit and is_even:
+        return True
+    return False
+
+print(is_even_two_digit_number(24)) #True
+print(is_even_two_digit_number(85))
