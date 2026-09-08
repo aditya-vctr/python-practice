5f8b44b85f17f635dceab01659a57e8fb2b96c57
aditya-vctr <adityaraj77716@gmail.com>
2026-09-08 23:41:54 +0530

    multiplication using recursion without using +,-,* operators

---
+++ practice/practice_34.py
@@ -0,0 +1,15 @@
+'''
+Write a recursive function named multiply accepts two positive
+integers 
+a and b as argument and returns their product. You can only 
+use + and - operators. You are not allowed to use the 
+* symbol anywhere in your code!
+'''
+
+def multiply(a,b):
+    if b == 1:
+        return a 
+    else:
+        return a + multiply(a, b-1)
+
+print(multiply(4,3))
